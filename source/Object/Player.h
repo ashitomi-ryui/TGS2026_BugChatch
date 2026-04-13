008a12bbf7655eb0435974ce1eb5ef3cd470dc1d
ashitomi-ryui <ryui-ashitomi-k25f@stu.kbc.ac.jp>
2026-04-13 11:07:49 +0900

    Playerの変数の追加

--- source/Object/Player.h
+++ source/Object/Player.h
@@ -1,19 +1,19 @@
 #pragma once
-
-
+#include "../Utilitys/Input.h"
+#include "../Utilitys/Math.h"
 
 struct Player
 {
 	int score;
-	//Vector2D location;
-	//Vector2D moveSpeed;
-	//Vector2D maxSpeed;
+	Vector2D location;	// ç¿ïW
+	Vector2D moveSpeed;	// ìÆÇ≠ë¨ìx
+	Vector2D maxSpeed;	// ç≈ëÂë¨ìx
 
 };
 
 struct BugNet
 {
-	//Vector2D stickLocation;
-	//Vector2D netLocation;
-	//Vector2D move;
+	Vector3D stickLocation;
+	Vector2D netLocation;
+	Vector2D move;
 };
