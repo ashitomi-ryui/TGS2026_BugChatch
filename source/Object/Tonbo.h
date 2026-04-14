2f0929148f50df7e597ffa6b71ae48f7c4a67397
naoto <naoto-hanaki-k25f@stu.kbc.ac.jp>
2026-04-14 23:04:04 +0900

    a

--- source/Object/Tonbo.h
+++ source/Object/Tonbo.h
@@ -2,7 +2,7 @@
 #ifndef WEIRD_MOTION_H
 #define WEIRD_MOTION_H
 
-void GetWeirdMotion(double t, int centerX, int centerY, int* x, int* y);
+void TonboMotion(double t, int centerX, int centerY, int* x, int* y);
 void TonboDraw(void);
 
 #endif
