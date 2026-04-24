690edd5412a07353df7397d101402c6651d45151
naoto <naoto-hanaki-k25f@stu.kbc.ac.jp>
2026-04-24 09:17:17 +0900

    butta

--- source/Object/Batta.cpp
+++ source/Object/Batta.cpp
@@ -26,12 +26,17 @@ void BattaInit(void)
 void BattaUpdate(float delta_second)
 {
 	BattaDestroy = TRUE;
+
 	static float vx = 0.0f;
 	static float vy = 0.0f;
 	static float Watitime = 0.0f;
-	float Gravity = 0.45f;
 
 	static bool Groundy = TRUE;
+	static bool UpMove = FALSE;      // 上昇中かどうか
+	static float JumpTopY = 0.0f;    // 今回のジャンプで止まるY座標
+
+	static bool TopStop = FALSE;     // 頂点で停止中
+	static float TopStopTimer = 0.0f;
 
 	time += delta_second;
 
@@ -40,7 +45,16 @@ void BattaUpdate(float delta_second)
 
 	if (now_batta - old_batta > 0)
 	{
-		Batta = { -100.0f,100.0f };
+		Batta = { -100.0f, 100.0f };
+
+		vx = 0.0f;
+		vy = 0.0f;
+		Watitime = 0.0f;
+		Groundy = TRUE;
+		UpMove = FALSE;
+		TopStop = FALSE;
+		TopStopTimer = 0.0f;
+
 		if (count == 0)
 		{
 			BattaDestroy = FALSE;
@@ -49,6 +63,7 @@ void BattaUpdate(float delta_second)
 		}
 
 		time1 += delta_second;
+
 		if (time1 >= 1.0f)
 		{
 			BattaDestroy = TRUE;
@@ -62,11 +77,13 @@ void BattaUpdate(float delta_second)
 		{
 		case 0:
 			groundyY = 690.0f;
+
 			if (battaspawn == 0)
 			{
-				Batta = { 100.0f,580.0f };
+				Batta = { 100.0f, 580.0f };
 				battaspawn = 1;
 			}
+
 			if (Groundy == TRUE)
 			{
 				Batta.y = groundyY;
@@ -75,40 +92,125 @@ void BattaUpdate(float delta_second)
 
 				Watitime += delta_second;
 
+				//1.5秒おきに動く
 				if (Watitime > 1.5f)
 				{
 					Watitime = 0.0f;
 					Groundy = FALSE;
+					UpMove = TRUE;
+					TopStop = FALSE;
+					TopStopTimer = 0.0f;
 
+					//50％の確率で左右のどちらかに行く
 					if (rand() % 2 == 0)
 					{
-						vx = 5.0f;
+						vx = 10.0f;
 					}
 					else
 					{
-						vx = -5.0f;
+						vx = -10.0f;
 					}
 
-					vy = -15.0f;
+					// 下の段用のランダムな頂点
+					JumpTopY = (float)(500 + rand() % 120);   // 500～619
+					vy = -8.0f;
 				}
-
 			}
 			else
 			{
-				vy += Gravity;
-
-				vx *= 0.995f;
-
-				Batta.x += vx;
-				Batta.y += vy;
-				if (Batta.y >= groundyY)
+				// 上昇中
+				if (UpMove == TRUE)
 				{
-					Batta.y = groundyY;
-					vy = 0.0f;
-					vx = 0.0f;
-					Groundy = TRUE;
+					Batta.x += vx;
+					Batta.y += vy;
+
+					// 上に向かっている時
+					if (vy < 0.0f)
+					{
+						if (Batta.y <= JumpTopY)
+						{
+							Batta.y = JumpTopY;
+							UpMove = FALSE;
+							TopStop = TRUE;
+							TopStopTimer = 0.0f;
+							vx = 0.0f;
+							vy = 0.0f;
+						}
+					}
+					// 下に向かっている時
+					else if (vy > 0.0f)
+					{
+						if (Batta.y >= JumpTopY)
+						{
+							Batta.y = JumpTopY;
+							UpMove = FALSE;
+							TopStop = TRUE;
+							TopStopTimer = 0.0f;
+							vx = 0.0f;
+							vy = 0.0f;
+						}
+					}
 				}
+				// 頂点で停止中
+				else if (TopStop == TRUE)
+				{
+						vx = 0.0f;
+						vy = 0.0f;
+					TopStopTimer += delta_second;
+
+					
+					Batta.y = JumpTopY;   // 固定
+
+					if (TopStopTimer >= 1.5f)
+					{
+						TopStop = FALSE;
+						TopStopTimer = 0.0f;   
+						// 次のジャンプ
+						UpMove = TRUE;
+
+						if (rand() % 2 == 0)
+						{
+							vx = 10.0f;
+						}
+						else
+						{
+							vx = -10.0f;
+						}
+						// たまに下がる
+						if (rand() % 2 == 0)   // 2回に1回くらい下がる
+						{
+							JumpTopY = Batta.y + (70 + rand() % 90);   // 70～159下がる
+						}
+						else
+						{
+							JumpTopY = Batta.y - (70 + rand() % 90);   // 70～159上がる
+						}
+
+						// 上に行きすぎ防止
+						if (JumpTopY < 50.0f)
+						{
+							JumpTopY = 50.0f;
+						}
+
+						// 下に行きすぎ防止
+						if (JumpTopY > 650.0f)
+						{
+							JumpTopY = 650.0f;
+						}
+						// 目標が今より下なら落ちる、上ならジャンプ
+						if (JumpTopY > Batta.y)
+						{
+							vy = 15.0f;    // 下へ
+						}
+						else
+						{
+							vy = -17.0f;   // 上へ
+						}
+					}
+				}
+				vx *= 0.995f;
 			}
+
 			if (Batta.x < 20.0f)
 			{
 				Batta.x = 20.0f;
@@ -118,13 +220,16 @@ void BattaUpdate(float delta_second)
 				Batta.x = 650.0f;
 			}
 			break;
+
 		case 1:
 			groundyY = 200.0f;
+
 			if (battaspawn == 1)
 			{
-				Batta = { 300.0f,200.0f };
+				Batta = { 350.0f, 200.0f };
 				battaspawn = 0;
 			}
+
 			if (Groundy == TRUE)
 			{
 				Batta.y = groundyY;
@@ -133,11 +238,15 @@ void BattaUpdate(float delta_second)
 
 				Watitime += delta_second;
 
+				//1.5秒おきに動く
 				if (Watitime > 1.5f)
 				{
 					Watitime = 0.0f;
 					Groundy = FALSE;
-
+					UpMove = TRUE;
+					TopStop = FALSE;
+					TopStopTimer = 0.0f;
+					//50%の確率で左右のどちらかに行くに行く
 					if (rand() % 2 == 0)
 					{
 						vx = 5.0f;
@@ -147,29 +256,119 @@ void BattaUpdate(float delta_second)
 						vx = -5.0f;
 					}
 
-					vy = -15.0f;
+					// 上の段用のランダムな頂点
+					JumpTopY = (float)(50 + rand() % 100);   // 50～149
+					vy = -8.0f;
 				}
-
 			}
 			else
 			{
-				vy += Gravity;
+				/*Batta.x += vx;*/
+
+				// 上昇中
+				if (UpMove == TRUE)
+				{
+					Batta.x += vx;
+					Batta.y += vy;
+
+					// 上に向かっている時
+					if (vy < 0.0f)
+					{
+						if (Batta.y <= JumpTopY)
+						{
+							Batta.y = JumpTopY;
+							UpMove = FALSE;
+							TopStop = TRUE;
+							TopStopTimer = 0.0f;
+							vx = 0.0f;
+							vy = 0.0f;
+						}
+					}
+					// 下に向かっている時
+					else if (vy > 0.0f)
+					{
+						if (Batta.y >= JumpTopY)
+						{
+							Batta.y = JumpTopY;
+							UpMove = FALSE;
+							TopStop = TRUE;
+							TopStopTimer = 0.0f;
+							vx = 0.0f;
+							vy = 0.0f;
+						}
+					}
+				}
+				// 頂点で停止中
+				else if (TopStop == TRUE)
+				{
+					vx = 0.0f;
+					TopStopTimer += delta_second;
+
+					Batta.y = JumpTopY;   // その場で固定
+					if (TopStopTimer >= 1.3f)
+					{
+						TopStop = FALSE;
+						TopStopTimer = 0.0f;
+						UpMove = TRUE;
+
+						if (rand() % 2 == 0)
+						{
+							vx = 5.0f;
+						}
+						else
+						{
+							vx = -5.0f;
+						}
+						
+						// たまに下がる
+						if (rand() % 3 == 0)   // 3回に1回くらい下がる
+						{
+							JumpTopY = Batta.y + ( 100+ rand() % 40);   // 100～139下がる
+						}
+						else
+						{
+							JumpTopY = Batta.y - (40 + rand() % 40);   // 40～79上がる
+						}
+
+						// 上に行きすぎ防止
+						if (JumpTopY < 30.0f)
+						{
+							JumpTopY = 30.0f;
+						}
+
+						// 下に行きすぎ防止
+						if (JumpTopY > 650.0f)
+						{
+							JumpTopY = 650.0f;
+						}
+						// 目標が今より下なら落ちる、上ならジャンプ
+						if (JumpTopY > Batta.y)
+						{
+							vy = 15.0f;    // 下へ
+						}
+						else
+						{
+							vy = -17.0f;   // 上へ
+						}
+
+						if (JumpTopY < 30.0f)
+						{
+							JumpTopY = 30.0f;
+						}
+							// 今いる位置から少しだけ上へジャンプ
+							//JumpTopY = Batta.y - (20.0f + rand() % 30);  // 20～49 上へ
+		
+					}
+				}
+				
 
 				vx *= 0.995f;
 
-				Batta.x += vx;
-				Batta.y += vy;
-				if (Batta.y >= groundyY)
-				{
-					Batta.y = groundyY;
-					vy = 0.0f;
-					vx = 0.0f;
-					Groundy = TRUE;
-				}
 			}
-			if (Batta.x < 300.0f)
+
+			if (Batta.x < 330.0f)
 			{
-				Batta.x = 300.0f;
+				Batta.x = 330.0f;
 			}
 			if (Batta.x > 1200.0f)
 			{
@@ -184,11 +383,10 @@ void BattaDraw(void)
 {
 	if (BattaDestroy == TRUE)
 	{
-		DrawCircle(Batta.x, Batta.y, 20, GetColor(255, 0, 255), TRUE);
+		DrawCircle((int)Batta.x, (int)Batta.y, 20, GetColor(255, 0, 255), TRUE);
 		DrawFormatString(100, 100, GetColor(255, 255, 255), "%f", time);
 	}
 }
-	
 
 Vector2D* BattaLocation(void)
 {
