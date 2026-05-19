0995c27438a6ae1cfed5f87d0c2db8198abc6686
ashitomi-ryui <ryui-ashitomi-k25f@stu.kbc.ac.jp>
2026-05-19 16:23:43 +0900

    制限時間の修正

--- source/Scene/InGameScene.cpp
+++ source/Scene/InGameScene.cpp
@@ -54,7 +54,7 @@ int InGameInit(void)//各プログラムの初期化
 eSceneType InGameUpdate(float delta_second)
 {
 	timer += delta_second;
-	if (timer > 1)
+	if (timer > 60.0f * 3.0f)
 	{
 		return eResult;//ゲーム終了時にタイトルに戻る（仮）
 	}
