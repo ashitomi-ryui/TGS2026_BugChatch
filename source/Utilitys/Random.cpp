a7669b43c7b6c7dd39bde120c138d19517a699a8
ashitomi-ryui <ryui-ashitomi-k25f@stu.kbc.ac.jp>
2026-05-22 11:50:04 +0900

    ランダム関数の変更

--- source/Utilitys/Random.cpp
+++ source/Utilitys/Random.cpp
@@ -9,13 +9,41 @@ void Random::SetSeed(float f)
 	seed = (int)(f * (float)0xfa2b) + rand();
 }
 
-int Random::GetRand()
+float Random::GetRand(float num1, float num2, float interval)
 {
-	seed = seed << 3 >> 4;
+	seed += rand();
 	seed += 0x6a23;
-	seed *= 0xfa2b;
-	seed = fabsf(seed);
-	//seed /= 3;
 	seed %= RAND_MAX;
-	return seed;
+	seed *= 0xfa2b;
+	seed = abs(seed);
+	seed %= RAND_MAX;
+
+	// 間隔を0にしない（0で割らないため）
+	if (interval == 0.0f)
+	{
+		interval = 1.0f;
+	}
+	// 値1を最小値に
+	if (num1 > num2)
+	{
+		float num = num1;
+		num1 = num2;
+		num2 = num;
+	}
+	// 出したいランダムな値の数がランダム最大値を越さないようにする
+	if ((num2 - num1) / interval > RAND_MAX)
+	{
+		num2 = num1 + RAND_MAX * interval;
+	}
+
+	// 0から出したいランダムな値の数までのランダムな値にする
+	float random = (float)seed;
+	random = (float)((int)random % (int)((num2 - num1) / interval));
+
+	// 間隔を空ける
+	random *= interval;
+	// 最小値分ずらす
+	random += num1;
+
+	return random;
 }
--- source/Utilitys/Random.h
+++ source/Utilitys/Random.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <DxLib.h>
 
 class Random
 {
@@ -7,5 +8,13 @@ public:
 
 public:
 	static void SetSeed(float f);
-	static int GetRand();
+
+	/// <summary>
+	/// ランダムな値を取得する
+	/// </summary>
+	/// <param name="num1">値1から</param>
+	/// <param name="num2">値2まで</param>
+	/// <param name="interval">間隔</param>
+	/// <returns>ランダムな値</returns>
+	static float GetRand(float num1 = (float)RAND_MAX, float num2 = 0.0f, float interval = 1.0f);
 };
