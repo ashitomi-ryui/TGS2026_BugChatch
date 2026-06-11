3b64c8f62b6262f7a5f15796aa64fe50b38facdc
ashitomi-ryui <ryui-ashitomi-k25f@stu.kbc.ac.jp>
2026-06-11 17:29:31 +0900

    ゲーム時間修正

--- source/Scene/InGameScene.cpp
+++ source/Scene/InGameScene.cpp
@@ -235,7 +235,7 @@ eSceneType InGameUpdate(float delta_second)
 	case 4:	// ==============================================ƒQ[ƒ€ƒvƒŒƒC
 		timer += delta_second;
 #ifndef _DEBUG
-		if (timer > 0.0f)
+		if (timer > 60.0f)
 		{
 			timer = 0.0f;
 
